cafd5b6fe3ba2e1b762eb63d0d3bafce2c7935bf
Jaimy <jaimy.putter@outlook.com>
2023-06-06 14:04:37 +0200

    Ontploffing is gefixed

--- week_6/week_6.py
+++ week_6/week_6.py
@@ -3,31 +3,30 @@ import matplotlib.pyplot as plt
 
 # Frequentie van drive mode (Hz)
 nu_drive = 4000
-# sense mode massa (mg)
-massa = 4.9e-06
-# dempingscoëfficiënt van sense mode (mg/s)
-gamma = 1.17
-# Geschaalde Coriolis kracht mN s/rad
-f_coriolis = 8e-09
+# sense mode massa (kg)
+massa = 4.9e-09
+# dempingscoëfficiënt van sense mode (kg/s)
+gamma = 1.17e-06
+# Geschaalde Coriolis kracht N s/rad
+f_coriolis = 8e-12
 
-k = 3.095e+03
+k = 3.095
 
 
 # resonantie frequentie = drive frequentie
-
 def force_coriolis_function(t):
     # Ω (rad / s)
     rotatie_snelheid = 10 / 60
     # (rad/s)
-    hoek_snelheid = 1
+    hoek_snelheid = 2 * np.pi * nu_drive
     return f_coriolis * rotatie_snelheid * np.cos(hoek_snelheid * t)
 
 
 def simulation_timeline():
     # Totale simulatieduur (s)
-    t_max = 10
+    t_max = 0.04
     # Tijdstap (aantal tijdstappen aanpassen voor gewenste nauwkeurigheid)
-    delta_time = 0.01
+    delta_time = 0.000000001
     return np.arange(0, t_max, delta_time), delta_time
 
 
@@ -65,7 +64,15 @@ def plot_simulation(t, uitwijking):
     plt.plot(t, uitwijking)
     plt.xlabel('Tijd (s)')
     plt.ylabel('Uitwijking (m)')
-    plt.xlim(2.5, 5)
+    plt.title('Uitwijking van de massa')
+    plt.show()
+
+def plot_f(t, kracht):
+    print("Totaal tijd: " + str(t[-1]))
+    # Plot de uitwijking van de massa
+    plt.plot(t, kracht)
+    plt.xlabel('Tijd (s)')
+    plt.ylabel('Kracht (m)')
     plt.title('Uitwijking van de massa')
     plt.show()
 
@@ -73,23 +80,25 @@ def plot_simulation(t, uitwijking):
 time_array, dt = simulation_timeline()
 coriolis_force_array = force_coriolis_function(time_array)
 
+# plot_f(time_array, coriolis_force_array)
+
 solved_differential_array = solve_differential_equation(time_array, dt, coriolis_force_array)
 
 
 print("Time: " + str(len(time_array)) + " vs " + str(len(solved_differential_array)))
-# plot_simulation(t=time_array, uitwijking=solved_differential_array)
+plot_simulation(t=time_array, uitwijking=solved_differential_array)
 
 print(solved_differential_array)
 
 
-# Get the time array and delta time
-time_array, delta_time = simulation_timeline()
-
-# Example data
-data = np.sin(2 * np.pi * 10 * time_array)
-
-# Plotting
-fig, ax = plt.subplots()
-ax.plot(time_array, solved_differential_array)
-
-plt.show()
+# # Get the time array and delta time
+# time_array, delta_time = simulation_timeline()
+#
+# # Example data
+# data = np.sin(2 * np.pi * 10 * time_array)
+#
+# # Plotting
+# fig, ax = plt.subplots()
+# ax.plot(time_array, solved_differential_array)
+#
+# plt.show()
